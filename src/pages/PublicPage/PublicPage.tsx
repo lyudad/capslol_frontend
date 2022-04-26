8558a5edaa946a489ce84e7efa3c855b2cb3e13e
Misha Krasnonos <muzishutka7@gmail.com>
2022-04-26 20:12:24 +0300

    up

--- src/pages/PublicPage/PublicPage.tsx
+++ src/pages/PublicPage/PublicPage.tsx
@@ -1,12 +1,32 @@
-import { Description, ProfileContainer, Title, TitleContainer } from "./styles";
+import {
+  Description,
+  ProfileContainer,
+  Avatar,
+  Title,
+  TitleContainer,
+} from "./styles";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 import avatar from "./avatar.png";
 
 const PublicPage: React.FC = () => {
   return (
     <ProfileContainer>
       <h1>User Name</h1>
+      <Avatar>
+        <img src={avatar} alt="" width={240} />
+      </Avatar>
 
-      <img src={avatar} alt="" />
+      <Button
+        type="primary"
+        style={{
+          position: "absolute",
+          bottom: 10,
+          right: 10,
+        }}
+      >
+        Settings
+      </Button>
     </ProfileContainer>
   );
 };
--- src/pages/PublicPage/styles.ts
+++ src/pages/PublicPage/styles.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
-import { colors } from "constants/index";
 
 export const ProfileContainer = styled.section`
   display: flex;
   margin-right: auto;
   margin-left: auto;
   margin-top: 12px;
-  max-width: 1200px;
-  height: 800px;
+  max-width: 800px;
+  height: 100vh;
   padding: 0px 12px;
   border: solid;
+  position: relative;
 `;
 
 export const TitleContainer = styled.div`
@@ -23,4 +23,10 @@ export const Title = styled.h1`
 
 export const Description = styled.p`
   text-align: center;
+`;
+export const Avatar = styled.div`
+  display: block;
+  position: absolute;
+  right: 0;
+  border: solid 1px tomato;
 `;
