3ea6d7ec4e811fb589b45059b4fb813f006412b4
sapar6ek0v <razetka0309@gmail.com>
2022-05-06 18:45:57 +0300

    fix

--- src/redux/models/passwordModels/password.model.ts
+++ src/redux/models/passwordModels/password.model.ts
@@ -1,11 +1,6 @@
 ﻿export interface Email {
   email: string;
 }
-
-export interface Token {
-  token: string | undefined;
-}
-
 export interface Password {
   token: string | undefined;
   password: string;
