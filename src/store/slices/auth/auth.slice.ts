67ba3c0f8318a8793dc9e32d2ff87b40ce958bf8
sapar6ek0v <razetka0309@gmail.com>
2022-05-10 13:48:19 +0300

    error

---
+++ src/store/slices/auth/auth.slice.ts
@@ -0,0 +1,35 @@
+﻿import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IResponse, UserType } from "./auth.types";
+
+interface UsersState {
+  user: UserType | null;
+  accessToken: string;
+  loading: "idle" | "loading" | "succeeded" | "failed";
+  error: string;
+  isLoggedIn: boolean;
+}
+const initialState: UsersState = {
+  user: {},
+  accessToken: "",
+  loading: "idle",
+  error: "",
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (
+      state,
+      { payload: { data } }: PayloadAction<IResponse>
+    ) => {
+      state.user = data.user;
+      state.accessToken = data.accessToken;
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
