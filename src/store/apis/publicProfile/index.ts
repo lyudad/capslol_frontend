bc92c924301d210b0e4f2f7481282a03fb66b452
Misha Krasnonos <muzishutka7@gmail.com>
2022-05-16 13:26:14 +0300

    fix coments from Lyuda

--- src/store/apis/publicProfile/index.ts
+++ src/store/apis/publicProfile/index.ts
@@ -1,42 +1,17 @@
-import { baseApi } from "..";
-import {
-  Educations,
-  Experiences,
-  Profile,
-  Skillls,
-} from "./publicProfile.types";
+import { baseApi } from '..';
+import { Profile } from './publicProfile.types';
 
 export const publicProfileApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    searchUser: builder.query<Profile, number | undefined>({
-      query: (profileType) => ({
-        url: `profiles/getById`,
-        params: {
-          user: profileType,
-        },
-      }),
+    endpoints: (builder) => ({
+        searchUser: builder.query<Profile, number | undefined>({
+            query: (profileType) => ({
+                url: `profiles/getById`,
+                params: {
+                    user: profileType,
+                },
+            }),
+        }),
     }),
-    searchExperience: builder.query<Experiences, number | undefined>({
-      query: (id) => ({
-        url: `experiences/${id}`,
-      }),
-    }),
-    searchEducations: builder.query<Educations, number | undefined>({
-      query: (id) => ({
-        url: `educations/${id}`,
-      }),
-    }),
-    searchSkills: builder.query<Skillls, number | undefined>({
-      query: (id) => ({
-        url: `skills/${id}`,
-      }),
-    }),
-  }),
 });
 
-export const {
-  useSearchUserQuery,
-  useSearchExperienceQuery,
-  useSearchEducationsQuery,
-  useSearchSkillsQuery,
-} = publicProfileApi;
+export const { useSearchUserQuery } = publicProfileApi;
--- src/store/apis/publicProfile/publicProfile.types.ts
+++ src/store/apis/publicProfile/publicProfile.types.ts
@@ -1,29 +1,39 @@
-﻿export interface Profile {
-  id: number;
-  profileImage: string;
-  hourRate: number;
-  availableHours: number;
-  position: string;
-  english: string;
-  other: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-export interface Experiences {
-  id: number;
-  companyName: string;
-  position: string;
-  startAt: string;
-  endAt: string;
+﻿export interface Experiences {
+    id: number;
+    companyName: string;
+    position: string;
+    startAt: string;
+    endAt: string;
 }
 export interface Educations {
-  id: number;
-  name: string;
-  specialization: string;
-  startAt: string;
-  endAt: string;
+    id: number;
+    name: string;
+    specialization: string;
+    startAt: string;
+    endAt: string;
 }
-export interface Skillls {
-  id: number;
-  name: string;
+export interface Category {
+    id: number;
+    categoryName: string;
+}
+
+export interface Skills {
+    id: number;
+    name: string;
+}
+
+export interface Profile {
+    id: number;
+    profileImage: string;
+    hourRate: number;
+    availableHours: number;
+    position: string;
+    experiense: Experiences;
+    educations: Educations;
+    categories: Category;
+    skills: Skills;
+    english: string;
+    other: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
