a2b687820918751c48b0fb775f938d44ad1b2f9c
Mo45 <ivioninjs@gmail.com>
2022-05-20 22:38:34 +0600

    implemented route protection

---
+++ src/router/Protected.tsx
@@ -0,0 +1,16 @@
+import { useAppSelector } from 'hooks/redux';
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { Paths } from './paths';
+
+const Protected: React.FC = () => {
+    const location = useLocation();
+    const isAuthenticated = useAppSelector(
+        (state) => state.authReducer.accessToken
+    );
+    if (isAuthenticated) {
+        return <Outlet />;
+    }
+    return <Navigate to={Paths.HOME} state={{ from: location.pathname }} />;
+};
+
+export default Protected;
