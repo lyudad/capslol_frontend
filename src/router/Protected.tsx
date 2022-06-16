15d6a76b09c8092eadb5185fd173448c5b66a6fe
Ivan Shvets <ivan.shvets@edisen.com>
2022-06-16 14:42:02 +0300

    created branch

--- src/router/Protected.tsx
+++ src/router/Protected.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable react/jsx-no-useless-fragment */
 import { useAppSelector } from 'hooks/redux';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import { Paths } from './paths';
 
-const Protected: React.FC = () => {
+interface IProps {
+    userRole: string;
+    redirectPath: string;
+    children: React.ReactNode;
+}
+
+export const Protected: React.FC = () => {
     const location = useLocation();
     const isAuthenticated = useAppSelector((state) => state.auth.accessToken);
     if (isAuthenticated) {
@@ -11,4 +19,36 @@ const Protected: React.FC = () => {
     return <Navigate to={Paths.HOME} state={{ from: location.pathname }} />;
 };
 
-export default Protected;
+// export const ProtectedFreelancer: React.FC<IProps> = ({
+//     children,
+// }): JSX.Element => {
+//     const location = useLocation();
+//     const role = useAppSelector((state) => state.auth.user?.role);
+//     if (role === userRole.freelancer) {
+//         return <>{children}</>;
+//     }
+//     return <Navigate to={Paths.HOME} state={{ from: location.pathname }} />;
+// };
+
+// export const ProtectedJobOwner: React.FC<IProps> = ({
+//     children,
+// }): JSX.Element => {
+//     const location = useLocation();
+//     const role = useAppSelector((state) => state.auth.user?.role);
+//     if (role === userRole.owner) {
+//         return <>{children}</>;
+//     }
+//     return <Navigate to={Paths.HOME} state={{ from: location.pathname }} />;
+// };
+
+export const ProtectedRoute: React.FC<IProps> = ({
+    userRole,
+    redirectPath,
+    children,
+}: IProps): JSX.Element => {
+    const role = useAppSelector((state) => state.auth.user?.role);
+    if (role !== userRole) {
+        return <Navigate to={redirectPath} replace />;
+    }
+    return <>{children}</>;
+};
